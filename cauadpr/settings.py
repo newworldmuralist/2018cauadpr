aa7eae7d882f482db4aaec687b5900e4105eafaa
newworldmuralist <mildjay16@gmail.com>
2018-07-28 22:08:45 +0900

    1807282208
    d

--- cauadpr/settings.py
+++ cauadpr/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r)oh#d&gv(^gk2ib2!mk@y(*hu0(5a_)5-&umv9m4#1ac$x5fv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['20sadluv2018.ap-northeast-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
